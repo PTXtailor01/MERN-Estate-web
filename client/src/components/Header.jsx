fe25b24c6e27661273f7d16f7bd9688218d02ef8
Kalp <131288303+Kalp0507@users.noreply.github.com>
2023-12-04 05:07:50 +0530

    Crete Header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {FaSearch} from 'react-icons/fa'
 
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+          <Link to={'/'}>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-400'>Real</span>
+            <span className='text-slate-500'>Real</span>
             <span className='text-slate-600'>Estate</span>
         </h1>
-        <form action="">
-            <input type="text" placeholder='Search...' />
+          </Link>
+        <form action="" className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+            <FaSearch className='text-slate-600'></FaSearch>
         </form>
+        <ul className='flex gap-5'>
+          <Link to={'/'}>
+          <li className='hidden sm:inline text-slate-700 hover:underline font-semibold'>Home</li>
+          </Link>
+          <Link to={'/about'}>
+          <li className='hidden text-slate-700 hover:underline sm:inline font-semibold'>About</li>
+          </Link>
+          <Link to={'/sing-in'}>
+          <li className=' text-slate-700 hover:underline sm:inline font-semibold'>Sing in</li>
+          </Link>
+        </ul>
         </div>
     </header>
   )
 }
+
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}'
+  ],
   theme: {
     extend: {},
   },
