1c4cb2f67507a36d3eaf10382c3bf53e4d43a294
Kalp <131288303+Kalp0507@users.noreply.github.com>
2023-12-05 04:26:59 +0530

    Create server and connect to database

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+const app =express();
+
+dotenv.config();
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=>{
+        console.log("Connected to mongodb")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+
+app.listen(3030 , ()=>{
+    console.log("App is running on port 3030.")
+})
